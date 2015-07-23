63f23668c3022d6c0cfd8a232a50c56fb2b3e5a2
Justin Grant <jlgrock@gmail.com>
2015-07-23 16:06:06 -0400

    [SNPPROT-147] adding ps check to verify that docker is running

--- release.groovy
+++ release.groovy
@@ -65,8 +65,11 @@ def downloadFiles() {
 
 def buildAndReleaseDockerImage(String snpVersion) {
 	println()
-	println "Building/Pushing docker images for latest and $snpVersion"
+	println "Check that the Docker image is running and who it is running as..."
 	executeCommand("whoami")
+	executeCommand("ps aux | grep docker")
+
+	println "Building/Pushing docker images for latest and $snpVersion"
 	executeCommand("docker build -t deloitteva/docker-snp:$snpVersion .")
 	executeCommand("docker tag -f deloitteva/docker-snp:$snpVersion jlgrock/docker-snp:latest")
 	if (!snpVersion.contains("-SNAPSHOT"))
